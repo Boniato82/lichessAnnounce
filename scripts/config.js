cbd981e0643aecbf59f442021b1a7090ffe084fd
Boniato82 <frikidune@gmail.com>
2016-04-03 08:59:27 +0200

    Añadida función para "juega"

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,12 @@
 
+$( '#juega' ).change(function() {
+    $( 'select option:selected' ).each(function() {
+        chrome.storage.sync.set({'juega': $( this )[0].value}, function() {
+            message('Juega ' + $( this )[0].value + ' set');
+        });
+    });
+});
+
 $( '#enabled' ).change(function() {
     chrome.storage.sync.set({'enabled': $( this ).prop('checked')}, function() {
         message('Enabled ' + $( this ).prop('checked') + ' set');
@@ -12,4 +20,10 @@ chrome.storage.sync.get('enabled', function(items) {
     else {
         $( '#enabled' ).bootstrapToggle('off');
     }
+});
+
+
+chrome.storage.sync.get('juega', function(items) {
+    juega = items['juega'];
+    $( '#juega' ).val(juega);
 });
